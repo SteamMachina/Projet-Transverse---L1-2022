2a798d495e79beb6e8a01b3a1090e7401a5b8a58
Nephrys <emylim175@gmail.com>
2022-05-19 00:30:47 +0200

    assets update
    Very rough reorganization of the assets, incomplete for now
    -> For tiled maps

---
+++ assets/Tiled/tiles_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tiles_set" tilewidth="30" tileheight="30" tilecount="35" columns="7">
+ <image source="tiles_set.png" width="210" height="150"/>
+</tileset>
